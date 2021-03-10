5cb31f7eb4dc570de845819a59d17ce606a49dcb
AlexSonar <alex.sonars@gmail.com>
2021-03-10 10:02:20 +0300

    Bastion resources added

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Datacenter_with_Terraform_and_Counsul
-test
-Datacenter with Terraform and Counsul as [ Bastion + Consull + Nomad + Nginx ] configuration
+
+
+
+## Datacenter infrastructure on AWS with Terraform and Counsul as [ Bastion + Consull + Nomad + Nginx ] configuration.
+
+It is demo project of a single-host webserver using consul
+
+This tutorial describes how to use Consul and Consul template to automatically update an NGINX configuration file with the latest list of backend servers using Consul's service discovery.
+
+
---
+++ bastion/main.tf
@@ -0,0 +1,51 @@
+##########################
+# Creating a for Bastion #
+##########################
+
+resource "aws_instance" "bastion" {
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  key_name                = var.key_name
+  subnet_id               = var.bastion_public_subnet
+  vpc_security_group_ids  = [aws_security_group.bastion.id]
+
+  tags = {
+    Name = "bastion"
+    role = "bastion"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file("keys/${var.key_name}.pem")
+      timeout     = "15m"
+    }
+    inline = [
+      "sudo DEBIAN_FRONTEND=noninteractive apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y sshguard"
+    ]
+  }
+}
+
+##################################
+# AWS security group for Bastion #
+##################################
+resource "aws_security_group" "bastion" {
+  name   = "bastion"
+  vpc_id = var.vpc_id
+
+  # SSH allowed from the Internet
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
