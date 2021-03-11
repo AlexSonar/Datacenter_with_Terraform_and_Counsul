def9f20d06e0e15f75d2243d91de38d00f915027
AlexSonar <alex.sonars@gmail.com>
2021-03-11 11:47:20 +0300

    Infrastructure

---
+++ infrastructure/infrastructure.tf
@@ -0,0 +1,52 @@
+# This creates a bastion host
+module "bastion" {
+  source = "./bastion"
+  bastion_public_subnet     = aws_subnet.public.id
+  ami                       = var.base_ec2_ami
+  instance_type             = "t2.micro"
+  key_name                  = "tutorialinux"
+  vpc_id                    = aws_vpc.tutorialinux.id
+}
+
+# This creates a consul cluster
+module "consul" {
+  source = "./consul"
+
+  # Arch ebs-hvm-x86_64-stable for us-west-2 from https://www.uplinklabs.net/projects/arch-linux-on-ec2/
+  ami                       = var.base_ec2_ami
+  instance_type             = "t2.micro"
+  azs                       = "us-west-2a,us-west-2b,us-west-2c"
+  consul_cluster_size       = 3
+  key_name                  = "tutorialinux"
+  name                      = "tutorialinux-consul"
+  subnet_id                 = aws_subnet.private.id
+  vpc_id                    = aws_vpc.tutorialinux.id
+  vpc_cidr                  = aws_vpc.tutorialinux.cidr_block
+  bastion_connect           = module.bastion.bastion_public_ip
+}
+
+# This creates a nomad cluster
+module "nomad" {
+  source = "./nomad"
+
+  # Arch ebs-hvm-x86_64-stable for us-west-2 from https://www.uplinklabs.net/projects/arch-linux-on-ec2/
+  ami                       = var.base_ec2_ami
+  instance_type             = "t2.micro"
+  azs                       = "us-west-2a,us-west-2b,us-west-2c"
+  nomad_cluster_size        = 3
+  key_name                  = "tutorialinux"
+  name                      = "tutorialinux-nomad"
+  subnet_id                 = aws_subnet.private.id
+  vpc_id                    = aws_vpc.tutorialinux.id
+  vpc_cidr                  = aws_vpc.tutorialinux.cidr_block
+}
+
+# This instantiates an nginx host, running the consul agent and reading from the consul KV store
+module "nginx" {
+  source = "./nginx"
+  ami                       = var.base_ec2_ami
+  instance_type             = "t2.micro"
+  key_name                  = "tutorialinux"
+  subnet_id                 = aws_subnet.public.id
+  vpc_id                    = aws_vpc.tutorialinux.id
+}
--- variables/variables.tf
+++ variables/variables.tf
@@ -1,3 +1,6 @@
+####################
+# Create variables #
+####################
 variable "name" {}
 variable "ami" {}
 variable "instance_type" {}
@@ -18,4 +21,26 @@ variable "nomad_cluster_size" {
 }
 variable "consul_version" {
     default = "1.9.1"
+}
+variable "private_subnet_cidr" {
+  type            = string
+  description     = "The CIDR for our private subnet"
+  default         = "10.0.1.0/24"
+}
+variable "public_subnet_cidr" {
+  type            = string
+  description     = "The CIDR for our public subnet"
+  default         = "10.0.10.0/24"
+}
+variable "public_subnet_az" {
+  type            = string
+  default         = "us-west-2a"
+}
+variable "private_subnet_az" {
+  type            = string
+  default         = "us-west-2b"
+}
+variable "base_ec2_ami" {
+  # us-west-2 ubuntu 20.04 HVM SSD
+  default = "ami-07dd19a7900a1f049"
 }
